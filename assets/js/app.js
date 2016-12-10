a3a9938c1688fe325ffc5321d5d365a45dd30cf4
Aisha <aishaprograms@gmail.com>
2016-12-10 09:18:59 -0600

    Completed app, updates every minute

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,27 +3,34 @@ var database = firebase.database();
 //Current time in military time
 var currentTime;
 
+//Function calls
 updateTime();
-
 setInterval(updateTime, 1000);
 $('#add-train-button').on('click', function() {
-    var name = $("#add-train-name").val().trim();
-    var destination = $("#add-destination").val().trim();
-    var trainTime = $("#add-train-time").val().trim();
-    var frequency = parseInt($("#add-frequency").val().trim());
-    //push the values to the database
-    database.ref().push({
-        name: name,
-        destination: destination,
-        trainTime: trainTime,
-        frequency: frequency,
-        timeAdded: firebase.database.ServerValue.TIMESTAMP
-    });
+    if (validateInputs()) {
+        var name = $("#add-train-name").val().trim();
+        var destination = $("#add-destination").val().trim();
+        var trainTime = $("#add-train-time").val().trim();
+        var frequency = parseInt($("#add-frequency").val().trim());
+        //push the values to the database
+        database.ref().push({
+            name: name,
+            destination: destination,
+            trainTime: trainTime,
+            frequency: frequency,
+            timeAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+        $('#add-train-name').val('');
+        $('#add-destination').val('');
+        $('#add-train-time').val('');
+        $('#add-frequency').val('');
+    }
     // Don't refresh the page!
     return false;
 });
-
 updateSchedule();
+//Schedule updates every minute
+setInterval(updateSchedule, 60 * 1000);
 
 //Function definitions
 function updateTime() {
@@ -32,12 +39,29 @@ function updateTime() {
     $('#time-update').text(currentTime);
 }
 
+function validateInputs() {
+    var isValid = true;
+    if ($("#add-train-name").val() === '') {
+        $('#train-modal').modal('open');
+        isValid = false;
+    } else if ($("#add-destination").val() === '') {
+        $('#train-modal').modal('open');
+        isValid = false;
+    } else if ($("#add-train-time").val() === '') {
+        $('#train-modal').modal('open');
+        isValid = false;
+    } else if ($("#add-frequency").val() === '') {
+        $('#train-modal').modal('open');
+        isValid = false;
+    }
+    return isValid;
+}
+
 function updateSchedule() {
+    $('#schedule-tbody').html('');
     database.ref().on("child_added", function(childSnapshot) {
         // Train time converted
         var trainTimeConverted = moment(childSnapshot.val().trainTime, "HH:mm");
-        //Checks to see train time is before current time
-        var trainTimeBeforeCurrent = trainTimeConverted.isBefore(currentTime);
         // Difference between the times
         var diffTime = moment().diff(moment(trainTimeConverted), "minutes");
         // Time apart (remainder)
